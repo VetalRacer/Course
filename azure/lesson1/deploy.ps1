08c7b89180f9788c7d7643ca9ea72f9894309396
Vitalii Brazhaev <vitalii_brazhaev@epam.com>
2021-07-08 21:16:45 +0400

    add comment

--- azure/lesson1/deploy.ps1
+++ azure/lesson1/deploy.ps1
@@ -1,12 +1,20 @@
+################### Variables #########################################
+
 $vaultName = "lesson1"
 $resourceGroup = "lesson1"
 $location = "westeurope"
 $secretName = "MySecret"
 $secretValue = ConvertTo-SecureString -String '$R3SSkDZ' -AsPlainText -Force
 
+
+################### Deploy a resource group ############################
+
 New-AzResourceGroup `
   -Name $resourceGroup `
   -Location $location
+
+
+################### Deploy Azure KeyVault ##############################
 
 New-AzKeyVault `
   -VaultName $vaultName `
@@ -14,15 +22,24 @@ New-AzKeyVault `
   -Location $location `
   -EnabledForTemplateDeployment
 
+
+################### Add secret in Azure KeyVault #######################
+
 Set-AzKeyVaultSecret `
   -VaultName $vaultName `
   -Name $secretName `
   -SecretValue $secretValue
 
+
+################### Deploy VM ##########################################
+
 New-AzResourceGroupDeployment `
   -ResourceGroupName $resourceGroup `
   -TemplateUri git\Course\azure\lesson1\template.json `
   -TemplateParameterFile parameters.json
+
+
+################### Deploy azure storage account #######################
 
 New-AzStorageAccount -ResourceGroupName $resourceGroup `
   -Name brazhaevcourse `
