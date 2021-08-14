d71cba5f6602fb2f0e541a0a70ab2f463e24d2f0
Vitaly <39149868+VetalRacer@users.noreply.github.com>
2021-08-15 00:57:41 +0400

    Delete docker/tack1 directory

--- docker/tack1/build.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-echo -e "\n\e[1;32mBuild first nginx image...\e[0m \n"
-docker build -f Dockerfile_nginx -t nginx.loc .
-echo -e "\n\e[1;32mBuild second nginx image...\e[0m \n"
-docker build -f Dockerfile_httpd -t httpd.loc .
